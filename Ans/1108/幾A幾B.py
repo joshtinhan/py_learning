43139052d3f6350cd1e022f7080ba256c182a9b2
Josh <>
2021-11-08 23:56:01 +0800

    Accepted answer

--- Ans/1108/幾A幾B.py
+++ Ans/1108/幾A幾B.py
@@ -1,16 +1,29 @@
+from functools import partial
+
+
 correct_ans_arr = [str(v) for v in input()]
 guess_arr = [str(v) for v in input()]
+dict_correct_ans_arr = dict()
+dict_guess_arr = dict()
 a = int(0)
-b = int(0)
-repeat_num = [None]
-correct_index = [None]
+b = int()
+correct_position = []
+correct_number_index = int()
 for i in range(len(correct_ans_arr)):
-    if correct_ans_arr[i] == guess_arr[i]:
+    dict_correct_ans_arr[i] = correct_ans_arr[i]
+    dict_guess_arr[i] = guess_arr[i]
+for i in dict_correct_ans_arr:
+    if dict_correct_ans_arr[i] == dict_guess_arr[i]:
         a += 1
-        correct_index.append(i)
-for j in range(len(guess_arr)):
-    for k in range(len(correct_ans_arr)):
-        if correct_ans_arr[k] == guess_arr[j] and guess_arr[j] not in repeat_num and j not in correct_index and k not in correct_index:
-            b += 1
-            repeat_num.append(guess_arr[j])
+        correct_position.append(i)
+for i in range(len(correct_position)):
+    dict_correct_ans_arr.pop(correct_position[i])
+    dict_guess_arr.pop(correct_position[i])
+for i in dict_guess_arr:
+    if dict_guess_arr[i] in dict_correct_ans_arr.values():
+        for j in dict_correct_ans_arr:
+            if dict_correct_ans_arr[j] == dict_guess_arr[i]:
+                correct_number_index = j
+        dict_correct_ans_arr.pop(correct_number_index)
+b = len(dict_guess_arr) - len(dict_correct_ans_arr)
 print(f"{a}A{b}B")
