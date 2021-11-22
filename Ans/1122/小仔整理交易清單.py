e0fada34249ebc678167619ffc9cd119a1da7576
Josh <>
2021-11-23 00:06:07 +0800

    交易清單 run time error

--- Ans/1122/小仔整理交易清單.py
+++ Ans/1122/小仔整理交易清單.py
@@ -1,15 +1,57 @@
-class store:
-    def __init__(self, item, price, customs=[]):
+class Store:
+    all = []
+
+    def __init__(self, item, price, history={}):
         self.item = item
         self.price = price
-        self.customs = customs
-    def buy(self,custom):
-        self.customs.append(custom)
+        self.history = history
+        Store.all.append(self)
+
+    def buy(self, name, quantity=0):
+        pass
+        if name in self.history:
+            self.history[name] += int(quantity)
+        else:
+            self.history[name] = int(quantity)
+
+    def __repr__(self) -> str:
+        return f"Store({self.item},{self.price},{self.history})"
+
+
 n, m = [v for v in input().split()]
-allInfo = []
 for i in range(int(n)):
     item, price = [v for v in input().split()]
-    allInfo.append(store(item,price))
-# item1 = store("apple",120)
-# item1.buy("Jeff")
-print(allInfo[0].item)
+    history = {}
+    Store(item, price, history)
+# for i in range(int(m)):
+for i in range(int(m)):
+    name, item, quantity = [v for v in input().split()]
+    for j in Store.all:
+        if j.item == item:
+            j.buy(name, quantity)
+# print(Store.all)
+while True:
+    try:
+        exec_func = input().split()
+        total = 0
+        if exec_func[0] == "A":
+            for i in Store.all:
+                if exec_func[1] in i.history:
+                    total += int(i.history[exec_func[1]]) * int(i.price)
+            print(total)
+        if exec_func[0] == "B":
+            for i in Store.all:
+                if i.item == exec_func[2]:
+                    if exec_func[1] in i.history:
+                        print(i.history[exec_func[1]])
+                    else:
+                        print(0)
+        if exec_func[0] == "C":
+            for i in Store.all:
+                if i.item == exec_func[1]:
+                    if i.history != {}:
+                        print(",".join(sorted(i.history.keys())))
+                    else:
+                        print(0)
+    except ValueError:
+        continue
